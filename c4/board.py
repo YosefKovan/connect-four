cfc852325f20952935c37043976f223da7c03e72
Yosef Kovan <154745088+YosefKovan@users.noreply.github.com>
2025-10-29 21:54:32 +0200

    adding the functions from the board and rules files

--- c4/board.py
+++ c4/board.py
@@ -1,6 +1,4 @@
 
-
-
 def create_board(cols=7, rows=6):
     """initializes the board"""
     mat = []
@@ -9,7 +7,6 @@ def create_board(cols=7, rows=6):
         for col in range(cols):
             mat_row.append('*')
 
-
 def column_is_full(board, col):
     """returns whether the whole column is full"""
     return board[0][col] != '*'
@@ -17,8 +14,37 @@ def column_is_full(board, col):
 def drop_disc(board, col, mark):
     """drops the disk if the column is not full"""
     if not column_is_full(board, col):
-        for row in reversed(board):
+        for row_index, row in enumerate(reversed(board)):
 
             if row[col] == '*':
                 row[col] = mark
-                return
+                return row_index, col
+
+def legal_moves(board):
+    """return a list with all the legal columns"""
+    legal_moves_arr = []
+    for col_index, val in enumerate(board[0]):
+        if val == '*':
+            legal_moves_arr.append(col_index)
+
+def create_col_numbers(num):
+    """this will create the column numbers on the top"""
+    row_numbers = ""
+    for i in range(num):
+        row_numbers += f"{i} "
+
+    row_numbers += '\n'
+    return row_numbers
+
+def render(board):
+    """returns a string of the board"""
+    row_numbers = create_col_numbers(len(board[0]))
+
+    render_str = ""
+    for row in board:
+        for val in row:
+            render_str += f"{val} "
+
+    render_str += '\n'
+
+    return row_numbers + render_str
---
+++ c4/rules.py
@@ -0,0 +1,54 @@
+
+def in_bounds(board, row, col):
+    """returns if the row and col are in the bounds of the board"""
+    return 0 <= row < len(board) and 0<= col < len(board[0])
+
+def check_direction(board, row, col, direction, mark):
+    """checks every direction """
+    #this will make sure that we don't start from the current piece
+    row += direction[0]
+    col += direction[1]
+
+    count = 0
+    while in_bounds(board, row, col):
+          if board[row][col] == mark:
+             count += 1
+             row, col = direction[0], direction[1]
+          else:
+              break
+
+    return count
+
+def check_all_directions(board, row, col, mark):
+    """checks all four directions if there is a - connect 4"""
+
+    #this will check horizontal direction
+    if check_direction(board, row, col, (0, 1), mark) + check_direction(board, row, col, (0, -1), mark) + 1 >= 4:
+        return True
+    #this will check vertical direction
+    if check_direction(board, row, col, (1, 0), mark) + check_direction(board, row, col, (-1, 0), mark) + 1 >= 4:
+        return True
+    #this will check the slant going from top left to bottom right
+    if check_direction(board, row, col, (-1, -1), mark) + check_direction(board, row, col, (-1, 1), mark) + 1 >= 4:
+        return True
+    #this will check the slant going from bottom left to top right
+    if check_direction(board, row, col, (1, 1), mark) + check_direction(board, row, col, (-1, 1), mark) + 1 >= 4:
+        return True
+
+def has_winner(board):
+    """returns if there is a winner"""
+    for row_index, row in enumerate(board):
+        for col_index in range(len(row)):
+            if check_all_directions(board, row_index, col_index, board[row_index][col_index]):
+                return True
+
+    return False
+
+def if_full(board):
+    """return if the board is full and there is nowhere else to go"""
+    for row_index, row in enumerate(board):
+        for col_index in range(len(row)):
+            if board[row_index][col_index] == '*':
+                return False
+
+    return True
